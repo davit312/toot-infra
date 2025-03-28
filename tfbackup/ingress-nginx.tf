6f0b6cab7e62f5334e0afe758767e0d5ae23fab1
Davit <davit_babayan@epam.com>
2025-03-28 19:44:05 +0400

    for future tests

---
+++ tfbackup/ingress-nginx.tf
@@ -0,0 +1,10 @@
+resource "helm_release" "ingress-nginx" {
+  name       = "ingress-nginx"
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart      = "ingress-nginx"
+
+  namespace        = "ingress-nginx"
+  create_namespace = true
+
+  depends_on = [ null_resource.config-kubectl ]
+}
---
+++ tfbackup/secrets.tf
@@ -0,0 +1,42 @@
+resource "aws_secretsmanager_secret" "app-encryption" {
+  name = "app-encription"
+}
+
+resource "aws_secretsmanager_secret" "db-secrets" {
+  name = "db-secrets"
+}
+
+resource "aws_secretsmanager_secret" "argocd-git-repo-key" {
+  name = "argocd-git-repo-key"
+}
+
+/** **/
+
+resource  "aws_secretsmanager_secret_version" "app-encryption" {
+  secret_id     = aws_secretsmanager_secret.app-encryption.id
+  secret_string = jsonencode(var.app-encryption)
+}
+
+resource  "aws_secretsmanager_secret_version" "db-secrets" {
+  secret_id     = aws_secretsmanager_secret.db-secrets.id
+  secret_string = jsonencode(var.db-secrets)
+}
+
+resource  "aws_secretsmanager_secret_version" "argocd-git-repo-key" {
+  secret_id     = aws_secretsmanager_secret.argocd-git-repo-key.id
+  secret_string = jsonencode(var.argocd-git-repo-key)
+}
+
+/** **/
+
+variable  "app-encryption" {
+  sensitive = true
+}
+
+variable "db-secrets" {
+  sensitive = true
+}
+
+variable "argocd-git-repo-key" {
+  sensitive = true
+}
